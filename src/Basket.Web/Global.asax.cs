e3e7c38e186709c0709fd8ba63db4f34ba4b9eda
Kijana Woodard <github@wyldeye.com>
2016-08-19 21:48:35 -0500

    initial commit

---
+++ src/Basket.Web/Global.asax.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Embedded;
+using Raven.Client.Indexes;
+using Raven.Client.NodaTime;
+using Raven.Database.Config;
+using Raven.Database.Server;
+using SimpleInjector;
+using SimpleInjector.Integration.Web;
+using SimpleInjector.Integration.Web.Mvc;
+
+namespace Basket.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RegisterContainer();
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void RegisterContainer()
+        {
+            var container = new Container();
+
+            RegisterRavenDB(container);
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+            container.RegisterMvcIntegratedFilterProvider();
+
+            container.Verify();
+
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+            //GlobalConfiguration.Configuration.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(container);
+        }
+
+        private void RegisterRavenDB(Container container)
+        {
+            NonAdminHttp.EnsureCanListenToWhenInNonAdminContext(8082);
+            var store = new EmbeddableDocumentStore
+            {
+                UseEmbeddedHttpServer = true,
+                DefaultDatabase = "baskets",
+                RunInMemory = true,
+                Configuration =
+                {
+                    Port = 8082
+                }
+            };
+
+            store.Configuration.Storage.Voron.AllowOn32Bits = true;
+
+            store.Conventions.DefaultUseOptimisticConcurrency = true;
+            store.Conventions.ShouldAggressiveCacheTrackChanges = true;
+            store.Conventions.ShouldSaveChangesForceAggressiveCacheCheck = false;
+            store.Conventions.MaxNumberOfRequestsPerSession = 100;
+            store.Conventions.FindTypeTagName = FindTypeTagName;
+            store.Initialize();
+            store.ConfigureForNodaTime();
+
+            container.Register<IDocumentStore>(() => store, Lifestyle.Singleton);
+            container.Register<IDocumentSession>(() => store.OpenSession(), new WebRequestLifestyle());
+
+            ExecuteIndexes(store);
+        }
+
+        private string FindTypeTagName(Type type)
+        {
+            return type.Namespace != null && type.Namespace.Contains(".Messaging.")
+                ? "Events"
+                : DocumentConvention.DefaultTypeTagName(type);
+        }
+
+        private void ExecuteIndexes(IDocumentStore store)
+        {
+            IndexCreation.CreateIndexes(Assembly.GetExecutingAssembly(), store);
+        }
+    }
+}
